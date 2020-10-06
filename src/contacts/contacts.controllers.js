5721335ae400ed744647516790079a1831fb6a1b
Pa30p42 <hajiev93@gmail.com>
2020-10-06 16:03:48 +0300

    in progress

--- src/contacts/contacts.controllers.js
+++ src/contacts/contacts.controllers.js
@@ -1,7 +1,26 @@
 const contacts = require("../../contacts");
+const fs = require("fs");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "../../db/contacts.json");
 
 exports.getContacts = async (req, res, next) => {
   res.status(200).send(await contacts.listContacts());
+};
+
+exports.addContacts = async (req, res, next) => {
+  try {
+    const contactsList = await listContacts();
+    const id = uuidv4();
+    const newContact = { id, name, email, phone };
+    await fsPromise.writeFile(
+      contactsPath,
+      JSON.stringify([...contactsList, newContact])
+    );
+    res.status(200);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 // module.exports = class ContactsController {
