01614dcca0283b7d55cc1aea8863462384219a27
Pa30p42 <hajiev93@gmail.com>
2020-10-16 20:58:20 +0300

    bug fixed with typesObjectId

--- src/contacts/contacts.controllers.js
+++ src/contacts/contacts.controllers.js
@@ -1,5 +1,6 @@
 const contacts = require("./contacts");
 const UserModel = require("./models");
+const mongoose = require("mongoose");
 
 exports.getContacts = async (req, res, next) => {
   try {
@@ -13,14 +14,16 @@ exports.getContacts = async (req, res, next) => {
 exports.getContactsById = async (req, res, next) => {
   try {
     const { id } = req.params;
+    let contact = "";
+    if (mongoose.Types.ObjectId.isValid(id)) {
+      contact = await UserModel.findById(id);
+      if (!contact) {
+        return res.status(404).json({ message: "Contact not found" });
+      }
 
-    const contact = await UserModel.findById(id);
-
-    if (!contact) {
-      return res.status(404).json({ message: "Contact not found" });
+      return res.status(200).send(contact);
     }
-
-    return res.status(200).send(contact);
+    return res.status(404).send("invalid id");
   } catch (err) {
     next(err);
   }
